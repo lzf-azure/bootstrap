b1ed72a1df2d277e8b2ca3cc787339b296d7aabd
Csaba Meszaros <abax.soraszem@gmail.com>
2022-04-05 12:54:56 +0200

    working up to TFE run

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # bootstrap
 Bootstrap scripts for LZ Project creation
 
-# Manual prerequisite steps
+# Manual prerequisite steps Github
 * manualy create GH Organization for the project (testt-projectid-name)
 * `export GH_ORGANIZATION=testt-projectid-name`
 * manually create the Github GH_PAT with sufficient OAUTH rights in Settings -> Developer Settings -> Personal Access Token
 * `export GH_PAT=ghp_lRydJ...`
-* manually create the Terraform Enterprise Organization Token TFE_ORG_TOKEN as Organization -> Settings -> API Token -> Create Organization Token
+
+# Manual setup TFE
+* create a new TFE Organization, eg `lzf-test-123`
+This will be the organization, where TFE will be hosting the workspaces for the GH repositories
+* create the Terraform Enterprise Organization Token TFE_ORG_TOKEN as Organization -> Settings -> API Token -> Create Organization Token.
+This token will allow the python script to access the TFE API on behalf of the TFE Organization
 * `export TFE_ORG_TOKEN=asdf.gsgf.gad`
+* create TFE connection to GH (OAUTH Token): TFE -> Organization Settings -> Providers -> Add VCS Provider -> github.com(custom). You will need to also create the GH Application to which it is linked, and copy the Client ID and Client Secret back to TFE. Ultimately, GH will issue and OAUTH token for TFE, which will be referenced by TFE_GH_OAUTH_TOKEN_ID in the bootstrap script. At this step, you will need to grant access to the GH organization created above. 
+Note: if the repo is forked, then the upstream organization must also grant access.
+* `export TFE_GH_OAUTH_TOKEN_ID=ot-...` 
 
 
 # Start the bootstrap script
@@ -19,5 +27,5 @@ pip install -r requirements.txt
 ```
 * and now run the bootstrapping
 ```bash
-./bootstrap.py -o testt-123-myproject -i 123 -n myproject -t lzf-test-123
+./bootstrap.py -g testt-123-myproject -i 123 -n myproject -t lzf-test-123
 ```
--- bootstrap.py
+++ bootstrap.py
@@ -16,9 +16,9 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logg
 logging.info(f"Bootstrapping started")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--organization", required=True)
 parser.add_argument("-i", "--projectId", required=True)
 parser.add_argument("-n", "--projectName", required=True)
+parser.add_argument("-g", "--ghOrganization", required=True)
 parser.add_argument("-t", "--tfeOrganization", required=True)
 args = parser.parse_args()
 
@@ -31,16 +31,18 @@ def setupGithub(args, current_timestamp):
     github = Github(config("GH_PAT"))
 
     # set the marker
-    logging.info(f"Adding repo description for organization {args.organization}")
-    projectOrg = github.get_organization(args.organization)
+    logging.info(f"Adding repo description for organization {args.ghOrganization}")
+    projectOrg = github.get_organization(args.ghOrganization)
     projectOrg.edit(description=f"Bootstrapped by LZF on {current_timestamp}")
 
     # fork the lzf-seed into this new org
     # find the reference to the repo to copy
     lzfSeedRepo = github.get_repo("lzf-azure/lzf-seed")
     projectLzfRepoName = f"lzf-{args.projectId}-{args.projectName}"
-    logging.info(f"will fork repository: {lzfSeedRepo.full_name} into : {args.organization}/{projectLzfRepoName}")
-    projectLzfRepo = lzfSeedRepo.create_fork(organization=args.organization)
+    logging.info(f"will fork repository: {lzfSeedRepo.full_name} into : {args.ghOrganization}/{projectLzfRepoName}")
+    # TODO: forking is not a good option, as TFE rights must be secured on the upstream repo as well
+    # Solution: use clone and push copy instead 
+    projectLzfRepo = lzfSeedRepo.create_fork(organization=args.ghOrganization)
 
     # set the properties of the new repo
     projectLzfRepo.edit(name=projectLzfRepoName)
@@ -48,13 +50,40 @@ def setupGithub(args, current_timestamp):
     projectLzfRepo.enable_vulnerability_alert()
 
 def setupTfe(args, current_timestamp):
-    logging.info(f"Setting up the TFE connection for {args.tfeOrganization}")
+    logging.info("Logging in to TFE org: %s", args.tfeOrganization)
     client = pyterprise.Client()
     client.init(config("TFE_ORG_TOKEN"), url='https://app.terraform.io')
     org = client.set_organization(id=args.tfeOrganization)
 
-    logging.info(f"Existing TFE Workspaces: {org.list_workspaces()}")
-    # workspaceName = f"lzf-{args.projectId}-{args.projectName}"
+    logging.info("Existing TFE Workspaces:  %s", len(org.list_workspaces()))
+    projectLzfRepoName = f"lzf-{args.projectId}-{args.projectName}"
+
+    vcs_options = {
+        "identifier": f"{args.ghOrganization}/{projectLzfRepoName}",
+        # "identifier": "abaxsoraszem/conntest",
+        "oauth-token-id": config("TFE_GH_OAUTH_TOKEN_ID"),
+        "branch": "main"
+    }
+
+    logging.info(f"Creating workspace for VCS {vcs_options}")
+    org.create_workspace(name=projectLzfRepoName,
+                        vcs_repo=vcs_options,
+                        auto_apply=False,
+                        queue_all_runs=False,
+                        working_directory='/',
+                        trigger_prefixes=['/'])
+    workspace = org.get_workspace(projectLzfRepoName)
+
+    logging.info("Adding variables to access github from terraform provider")
+    workspace.create_variable(key='GITHUB_TOKEN', value=config("GH_PAT"), sensitive=True, category='env')
+    workspace.create_variable(key='GITHUB_OWNER', value=args.ghOrganization, sensitive=False, category='env')
+    workspace.create_variable(key='projectId', value=args.projectId, sensitive=False, category='terraform')  
+    workspace.create_variable(key='projectName', value=args.projectName, sensitive=False, category='terraform')  
+
+    logging.info("Workspace runs: %s", len(workspace.list_runs()))
+
+    logging.info("Running intial TFE apply on workspace %s. Dont forget to apply manually", projectLzfRepoName)
+    workspace.run()
 
 
 
